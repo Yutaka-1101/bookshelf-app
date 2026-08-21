fd0d0df365a72b023ddcd6b1b623075797339cc3
Yutaka Takahashi <speedstar11010118@gmail.com>
2026-08-21 19:53:33 +0900

    feat: Fortifyによる認証機能の実装 #4

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+//仮ルート
+Route::middleware('auth')->group(function () {
+    Route::get('/', fn() => '書籍一覧（準備中）')->name('books.index');
 });
